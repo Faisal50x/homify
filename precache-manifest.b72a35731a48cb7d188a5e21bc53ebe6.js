ba6035c0c6fb957b256a51b3f927aaa07eb991e1
Faisal Ahmed <contact.faisalahmed@gmail.com>
2019-03-17 21:30:33 +0600

    Updates

---
+++ precache-manifest.b72a35731a48cb7d188a5e21bc53ebe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb893f3274ec7998a646",
+    "url": "/homify/static/css/main.3f4dc34e.chunk.css"
+  },
+  {
+    "revision": "fb893f3274ec7998a646",
+    "url": "/homify/static/js/main.e183b946.chunk.js"
+  },
+  {
+    "revision": "2991042e83adaa08703c",
+    "url": "/homify/static/js/runtime~main.6e357320.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/homify/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homify/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b4c27c7250ed38a98a0bbf6927bea3d5",
+    "url": "/homify/index.html"
+  }
+];
